6919a24b94c7f59fee06ec834f2009c3a395dd0d
Michael Hyer <michaelhy2002@gmail.com>
2024-09-19 21:31:29 -0600

    Completed w01 project

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mongodb = require("./data/database");
+const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+const port = process.env.PORT || 3000;
+
+app.use("/", require("./routes"));
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
+    }
 });
-
-const port = 3000;
-
-app.listen(process.env.port || port)
-console.log("Web Server is listening at port " + (process.env.port || port));
