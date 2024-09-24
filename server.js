03d50447c96700fb1aed76c422ab653865f3055b
Michael Hyer <michaelhy2002@gmail.com>
2024-09-24 12:50:53 -0600

    Fixed getSingle bug and added body-parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongodb = require("./data/database");
+const bodyParser = require("body-parser");
 const app = express();
 
 const port = process.env.PORT || 3000;
